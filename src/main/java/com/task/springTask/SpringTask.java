d33cb1a8d65d1fff503140901419b3830788b374
anaer <lvcunna@gmail.com>
2015-01-06 21:48:53 +0800

    添加定时任务注解配置

--- src/main/java/com/task/springTask/SpringTask.java
+++ src/main/java/com/task/springTask/SpringTask.java
@@ -8,11 +8,26 @@ public class SpringTask {
     // 注解驱动
     @Scheduled(cron = "0/2 * * * * ?")
     public void myTask() {
-	System.out.println("注解调用！");
+        System.out.println("注解调用！");
     }
 
     //配置文件驱动
     public void myTask2() {
-	System.out.println("配置文件调用！");
+        System.out.println("配置文件调用！");
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void myTask3() {
+        System.out.println("定时任务: @Scheduled(fixedRate = 5000), 每5000毫秒调用一次");
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    public void myTask4() {
+        System.out.println("定时任务: @Scheduled(fixedDelay=5000), 任务完成后5000毫秒执行下次任务 ");
+    }
+
+    @Scheduled(cron = "0 0,30 8-23 * * MON-FRI")
+    public void myTask5() {
+        System.out.println("定时任务: @Scheduled(cron=\"0 0,30 8-23 ? * MON-FRI\"), 工作日8点到23点,整点与半点的时候执行一次");
     }
 }
